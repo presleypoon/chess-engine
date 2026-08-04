ab2b33c49e0cc3bb7e638c190f2f2bb3b80fe221
Presley <presleypoon37@gmail.com>
2026-08-04 18:20:09 +0100

    Auto Update

---
+++ build.bat
@@ -0,0 +1,4 @@
+@echo off
+cargo run
+pause
+exit
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone)]
+enum Piece {
+	k,
+	q,
+	r,
+	n,
+	b,
+	p,
+	K,
+	Q,
+	R,
+	N,
+	B,
+	P,
+	None,
+}
+
+struct Game {
+	board: [[Piece; 8]; 8],
+}
+impl Game {
+	fn new() -> Self {
+		Game {
+			board: [
+				[
+					Piece::r,
+					Piece::n,
+					Piece::b,
+					Piece::q,
+					Piece::k,
+					Piece::b,
+					Piece::n,
+					Piece::r,
+				],
+				[Piece::p; 8],
+				[Piece::None; 8],
+				[Piece::None; 8],
+				[Piece::None; 8],
+				[Piece::None; 8],
+				[Piece::P; 8],
+				[
+					Piece::R,
+					Piece::N,
+					Piece::B,
+					Piece::Q,
+					Piece::K,
+					Piece::B,
+					Piece::N,
+					Piece::R,
+				],
+			],
+		}
+	}
+}
+
+fn window_conf() -> Conf {
+	Conf {
+		window_title: "Chess".to_string(),
+		fullscreen: true,
+		..Default::default()
+	}
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+	let game: Game = Game::new();
+
+	loop {
+		// render();
+
+		next_frame().await;
+	}
 }
